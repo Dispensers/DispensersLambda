8b06e7386580a84e11ba62ccad3493aeb09ad9e2
Dispensers <121032668+Dispensers@users.noreply.github.com>
2024-02-06 18:57:18 +0000

    Add files via upload

--- puzzleSpec.js
+++ puzzleSpec.js
@@ -1,4 +1,4 @@
-
+/*
 let punterPuzzleSpec = {
 	number: "Az16",
 	solveBy: "5 Feb",
@@ -9,95 +9,92 @@ let punterPuzzleSpec = {
 	solutionDispenseSequence: [2, 2, 3, 1, 3, 2, 3, 2, 2],
 	solutionColourSequence: ["Black", "Orange", "Orange", "Black", "Green", "Orange", "Black", "Green", "Orange"]
 };
+*/
 
-
-
+let punterPuzzleSpec = {
+	number: "Ca1",
+	solveBy: "12 Feb",
+	dispenserSpec: [undefined, "pqr","ppsq", "tr"],
+	matrixSpec: [-2, -4, 6, 9, -1, 8, 7, 5, -3],
+	colourSpec: ["Black", "Orange", "Blue", "Green", "Pink"],
+	hintSpec: ["Blue", 2],
+	solutionDispenseSequence: [2, 1, 2, 3, 2, 3, 2, 1, 1],
+	solutionColourSequence: ["Blue", "Blue", "Pink", "Black", "Orange", "Orange", "Black", "Black", "Green"]
+};
 
 /*
 let punterPuzzleSpec = {
+	number: "Cb1",
+	solveBy: "19 Feb",
+	dispenserSpec: [undefined, "pqqqrr","qp", "q"],
+	matrixSpec: [-1, -3, 5, 8, 9, 7, 6, -4, -2],
+	colourSpec: ["Black", "Green", "Blue"],
+	hintSpec: ["Orange", 2],
+	solutionDispenseSequence: [1, 3, 1, 1, 1, 2, 1, 1, 2],
+	solutionColourSequence: ["Blue", "Blue", "Green", "Black", "Green", "Green", "Black", "Green", "Green"]
+};
+*/
+/*
+let punterPuzzleSpec = {
 	number: "Gf1",
-	solveBy: "28 Jan",
+	solveBy: "26 Feb",
 	dispenserSpec: [undefined, "pqqqqrs","tq"],
 	matrixSpec: [-1, -4, 7, 6, 9, -2, -3, 8, 5],
 	colourSpec: ["Black", "Green", "Blue", "Pink", "Orange"],
 	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
-};
-*/
-/*
-let punterPuzzleSpec = {
-	number: "Cb1",
-	solveBy: "28 Jan",
-	dispenserSpec: [undefined, "pqqqrr","qp", "q"],
-	matrixSpec: [-1, -3, 5, 8, 9, 7, 6, 4, -2],
-	colourSpec: ["Black", "Green", "Blue"],
-	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
-};
-*/
-/*
-let punterPuzzleSpec = {
-	number: "Ca1",
-	solveBy: "28 Jan",
-	dispenserSpec: [undefined, "pqr","ppsq", "tr"],
-	matrixSpec: [-2, -4, 6, 9, -1, 8, 7, 5, -3],
-	colourSpec: ["Black", "Orange", "Blue", "Green", "Pink"],
-	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
-};
-*/
-/*
-let punterPuzzleSpec = {
-	number: "Ax1",
-	solveBy: "28 Jan",
-	dispenserSpec: [undefined, "pq","rp", "sqsrq"],
-	matrixSpec: [9, 8, 7, 6, 5, -4, -3, -2, -1],
-	colourSpec: ["Orange", "Blue", "Green", "Black"],
-	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
-};
-*/
-/*
-let punterPuzzleSpec = {
-	number: "F2",
-	solveBy: "28 Jan",
-	dispenserSpec: [undefined, "pqrqr","pq", "ps"],
-	matrixSpec: [-4, -3, 5, 6, -2, 7, 8, -1, 9],
-	colourSpec: ["Green", "Orange", "Blue", "Black"],
-	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
+	solutionDispenseSequence: [1, 2, 1, 1, 1, 2, 1, 1, 1],
+	solutionColourSequence: ["Pink", "Green", "Green", "Orange", "Black", "Green", "Blue", "Green", "Green"]
 };
 */
 /*
 let punterPuzzleSpec = {
 	number: "D2",
-	solveBy: "28 Jan",
+	solveBy: "4 Mar",
 	dispenserSpec: [undefined, "t","pqsr", "rppp"],
 	matrixSpec: [-1, -3, 6, -2, 5, 8, -4, 7, 9],
 	colourSpec: ["Black", "Blue", "Green", "Orange", "Pink"],
 	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
+	solutionDispenseSequence: [3, 3, 2, 2, 3, 3, 2, 2, 1],
+	solutionColourSequence: ["Black", "Green", "Green", "Black", "Black", "Black", "Orange", "Blue", "Pink"]
 };
 */
 /*
 let punterPuzzleSpec = {
-	number: 10,
-	solveBy: "28 Jan",
-	dispenserSpec: [undefined, "ps","rsr", "pqtq"],
-	//dispenserSpec: [undefined, "pqrpq", "pqrp"],
+	number: "Az17",
+	solveBy: "11 Mar",
+	dispenserSpec: [undefined, "sqppq","pqqr"],
 	matrixSpec: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-	colourSpec: ["Blue", "Black", "Green", "Orange", "Pink"],
-	hintSpec: ["Orange", 2],
-	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
-	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
+	colourSpec: ["Black", "Pink", "BBBBBBPP", "PPBBBBBB"],
+	hintSpec: ["Orange", 6],
+	solutionDispenseSequence: [1, 2, 1, 2, 2, 1, 1, 1, 2],
+	solutionColourSequence: ["Pink", "BBBBBBPP", "Black", "Pink", "Pink", "Black", "Pink", "PPBBBBBB", "Black"]
 };
 */
+/*
+let punterPuzzleSpec = {
+	number: "F2",
+	solveBy: "18 Mar",
+	dispenserSpec: [undefined, "pqrqr","pq", "ps"],
+	matrixSpec: [-4, -3, 5, 6, -2, 7, 8, -1, 9],
+	colourSpec: ["Green", "Orange", "Blue", "Black"],
+	hintSpec: ["Orange", 2],
+	solutionDispenseSequence: [3, 3, 1, 1, 1, 1, 1, 2, 2],
+	solutionColourSequence: ["Orange", "Blue", "Blue", "Orange", "Green", "Green", "Orange", "Black", "Green"]
+};
+*/
+/*
+let punterPuzzleSpec = {
+	number: "Ax1",
+	solveBy: "?? Apr",
+	dispenserSpec: [undefined, "pq","rp", "sqsrq"],
+	matrixSpec: [9, 8, 7, 6, 5, -4, -3, -2, -1],
+	colourSpec: ["Orange", "Blue", "Green", "Black"],
+	hintSpec: ["Orange", 2],
+	solutionDispenseSequence: [1, 1, 2, 3, 3, 3, 3, 2, 3],
+	solutionColourSequence: ["Black", "Green", "Blue", "Black", "Green", "Blue", "Orange", "Orange", "Blue"]
+};
+*/
+/* ============================================================================================================================ */
 
 
 
@@ -111,17 +108,5 @@ let punterPuzzleSpec = {
 	hintSpec: ["Orange", 2],
 	solutionDispenseSequence: [2, 2, 1, 2, 3, 1, 3, 3, 3],
 	solutionColourSequence: ["Green", "Orange", "Orange", "Green", "Black", "Blue", "Pink", "Black", "Blue"]
-};
-*/
-/*
-let punterPuzzleSpec = {
-	number: "Az??",
-	solveBy: "5 Feb",
-	dispenserSpec: [undefined, "sqppq","pqqr"],
-	matrixSpec: [1, 2, 3, 4, 5, 6, 7, 8, 9],
-	colourSpec: ["Black", "Pink", "BBBBBBPP", "PPBBBBBB"],
-	hintSpec: ["Orange", 6],
-	solutionDispenseSequence: [2, 2, 3, 1, 3, 2, 3, 2, 2],
-	solutionColourSequence: ["Black", "Orange", "Orange", "Black", "Green", "Orange", "Black", "Green", "Orange"]
 };
 */
